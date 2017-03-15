74ac6294180250ed3970c3fce11eb8668b901269
koarks <keer9624@gmail.com>
2017-03-15 23:50:29 +0530

    Update cat.php

--- cat.php
+++ cat.php
@@ -6,30 +6,10 @@ $db='book1';
 $pwd="e924a34e";
 $user="b399ff00c924be";
 
-//$con = mysql_connect('localhost',$username,$password) or die("Unable to log into database");
-//@mysql_select_db($database,$con) or die("Unable to connect");
  $conn = new mysqli($host, $user, $pwd, $db);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-
-//phpinfo();
-  // try {
-  //     $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-  //     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-  //      //PDOStatement::errorInfo();
-  //  }
-  //  catch(Exception $e){
-  //      die(var_dump($e));
-  //  }
-  // $connectionOptions = array(
-	 // "Database" => "catofbooks",
-  // 	 "Uid" => "b5fc840de6873a",
-  // 	 "PWD" => "2ab7a8e4");
-  // $conn = sqlsrv_connect($host, $connectionOptions);
-//if($conn){ echo "Connected!";}
 
 if(!empty($_POST['cat'])){
 	$inputcat = $_POST['cat'];
@@ -59,19 +39,11 @@ if(!empty($_POST['booksearch'])){
 	echo mysqli_errno();
 
 	$result = mysqli_query($conn,$query) or die(mysqli_errno());
-
-	// if($row = mysql_fetch_array($result) == FALSE){echo "False";}
-	// else{
-		// echo "Books Found<br><br>";
 		$row = mysqli_fetch_array($result);
-		//<!-- echo $row["Bname"] ."  "."<br><br>"; -->
 		 	echo $row["Bname"] ."<br> Price: ".$row["Price"]."<br> Copies: ". $row["Copies"]."<br>  Rating: ".$row["Rating"];?><form method="POST"><button formaction="purchase.php" type="submit" value = "<?php echo $row["Bname"] ?>" name = "Bname">Buy Book</button></form><br><br>
 	<br><form><button formaction="Client.php" type="submit">HOME</button></form></br>
 	<?php
  }
-//echo $result;
-
-//}
 mysqli_close($mysqli);
 
 ?>
