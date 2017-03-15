3ff9ff77d2e472b2dec7d1ef646da24b59c7795d
koarks <keer9624@gmail.com>
2017-03-15 22:52:25 +0530

    Update seller.php

--- seller.php
+++ seller.php
@@ -1,11 +1,32 @@
 <?php
 
-$database="book1";
-$password="";
-$username="root";
 
-$con = mysql_connect('localhost',$username,$password) or die("Unable to log into database");
-@mysql_select_db($database,$con) or die("Unable to connect");
+$host="http://mybookapplication.azurewebsites.net"
+$db='book1';
+$pwd="e924a34e";
+$user="b399ff00c924be";
+
+
+ $conn = new mysqli($host, $user, $pwd, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+ // try {
+ //     $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+ //     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ //  }
+ //  catch(Exception $e){
+ //      die(var_dump($e));
+ //  }
+ // $connectionOptions = array(
+	// "Database" => "catofbooks",
+ // 	 "Uid" => "b5fc840de6873a",
+ // 	 "PWD" => "2ab7a8e4");
+ // $conn = sqlsrv_connect($host, $connectionOptions);
+//if($conn){ echo "Connected!";}
+
 
 if(!empty($_POST['Bnam']) && !empty($_POST['Anam']) && !empty($_POST['Pric'])){
 $inputbname = $_POST['Bnam'];
@@ -18,13 +39,14 @@ $inputRating = $_POST['Ratin'];
 
 $query = "INSERT INTO details VALUES ('$inputbname','$inputAname','$inputPrice','$inputFormat','$inputCopies','$inputCategory','$inputRating')";
 
-echo mysql_error();
-$result = mysql_query($query) or die(mysql_error());
+echo mysqli_errno();
+$result = mysqli_query($conn,$query) or die(mysqli_errno());
 
 if($result == FALSE){echo "False";}
 else{
 	//$result = mysql_query($query);
 	//$row = mysql_fetch_array($result);
+
 	echo "You have added the book successfully<br>";?>
 	<br><form><button formaction="Seller.html" type="submit">HOME</button></form></br>
 <?php
@@ -35,11 +57,11 @@ if(!empty($_POST['bkname']) && !empty($_POST['newprice'])){
 	$bookname = $_POST['bkname'];
 $new_price = $_POST['newprice'];
 $query="UPDATE details SET Price='$new_price' WHERE bname='$bookname'";
-mysql_query($query) or die(mysql_error());
+mysqli_query($conn,$query) or die(mysqli_errno());
 echo "Book price has been successfully updated";?>
 <br><form><button formaction="Seller.html" type="submit">HOME</button></form></br>
 <?php
 }
 
-mysql_close();
+mysqli_close($conn);
 ?>
