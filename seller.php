eaccc1d2d0f9df4fbdac57aa797e4d42a9daaa8b
koarks <keer9624@gmail.com>
2017-03-15 23:55:37 +0530

    Update seller.php

--- seller.php
+++ seller.php
@@ -8,25 +8,9 @@ $user="b399ff00c924be";
 
 
  $conn = new mysqli($host, $user, $pwd, $db);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
- // try {
- //     $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
- //     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
- //  }
- //  catch(Exception $e){
- //      die(var_dump($e));
- //  }
- // $connectionOptions = array(
-	// "Database" => "catofbooks",
- // 	 "Uid" => "b5fc840de6873a",
- // 	 "PWD" => "2ab7a8e4");
- // $conn = sqlsrv_connect($host, $connectionOptions);
-//if($conn){ echo "Connected!";}
-
 
 if(!empty($_POST['Bnam']) && !empty($_POST['Anam']) && !empty($_POST['Pric'])){
 $inputbname = $_POST['Bnam'];
@@ -44,8 +28,6 @@ $result = mysqli_query($conn,$query) or die(mysqli_errno());
 
 if($result == FALSE){echo "False";}
 else{
-	//$result = mysql_query($query);
-	//$row = mysql_fetch_array($result);
 
 	echo "You have added the book successfully<br>";?>
 	<br><form><button formaction="Seller.html" type="submit">HOME</button></form></br>
